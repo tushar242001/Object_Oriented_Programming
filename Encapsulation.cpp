227a40ce5844a652307d300692fd12191851c2ca
Tushar Bapu Adling <110562490+tushar242001@users.noreply.github.com>
2023-12-03 12:37:53 +0530

    Update Encapsulation.cpp

--- Encapsulation.cpp
+++ Encapsulation.cpp
@@ -1,4 +1,13 @@
 // Encapsulation is the wrapping up data and information under a single unit 
+// Example of Encapsulation is :  In a company, there are different sections like the accounts section, finance section, sales section, etc.
+//                                Now, The finance section handles all the financial transactions and keeps records of all the data related to finance.
+//                                Similarly, the sales section handles all the sales-related activities and keeps records of all the sales.
+//                                Now there may arise a situation when for some reason an official from the finance section needs all the data 
+//                                about sales in a particular month.In this case, he is not allowed to directly access the data of the sales section. 
+//                                He will first have to contact some other officer in the sales section and then request him to give the particular data.
+// 
+
+
 
 #include<iostream>
 using namespace std;
@@ -24,7 +33,7 @@ int main()
     d.accept();
     d.display();
 }
-// cout<<d.x              We can not access x directly because x is private member of a class .
+// cout<<d.x              :  We can not access x directly because x is private member of a class .
 
 // Class is a blue print of object .
 //  Object is Real world entity .
